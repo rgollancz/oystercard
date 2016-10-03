51d8686ce7ab25adeea9af0dbb7426d23ebcd452
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-10-03 16:43:25 +0100

    top up method and test added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,11 @@
 require 'oystercard'
 
 describe Oystercard do
-  
   it 'has a balance of 0 by default' do
   expect(subject.balance).to eq 0
   end
 
+  it 'balance can be topped up' do
+    expect(subject.top_up(40)).to eq subject.balance
+  end
 end
